60fc69613f07ef432f55e9cdbe44c26586dfab85
malexis <miltiadis.alexis@cern.ch>
2021-01-31 13:10:46 +0100

    [constants] a place where constants live

---
+++ constants/constants.go
@@ -0,0 +1,13 @@
+/*
+ *
+ * Author: Miltiadis Alexis <alexmiltiadis@gmai.com>
+ *
+ */
+
+// Package constants provides some constants for use in Mikasa Project
+package constants
+
+const (
+	BotName = "Levi Ackerman" // The Bot's name
+	Version = "0.0.1"         // The Bot's version.
+)
