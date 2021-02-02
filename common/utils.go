aed8614797786741b386ef48625f4a9de1c05151
malexis <miltiadis.alexis@cern.ch>
2021-02-02 22:28:21 +0100

    [common] add utils

--- common/utils.go
+++ common/utils.go
@@ -7,6 +7,8 @@
 package common
 
 import (
+	"strings"
+
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -49,4 +51,51 @@ func AddField(name, value string) *discordgo.MessageEmbedField {
 		Value: value,
 	}
 
+}
+
+// StringArrayContains checks whether or not the given string array contains the given string
+func StringArrayContains(array []string, str string, ignoreCase bool) bool {
+	if ignoreCase {
+		str = strings.ToLower(str)
+	}
+	for _, value := range array {
+		if ignoreCase {
+			value = strings.ToLower(value)
+		}
+		if value == str {
+			return true
+		}
+	}
+	return false
+}
+
+// StringHasPrefix checks whether or not the string contains one of the given prefixes and returns the string without the prefix
+func StringHasPrefix(str string, prefixes []string, ignoreCase bool) (bool, string) {
+	for _, prefix := range prefixes {
+		stringToCheck := str
+		if ignoreCase {
+			stringToCheck = strings.ToLower(stringToCheck)
+			prefix = strings.ToLower(prefix)
+		}
+		if strings.HasPrefix(stringToCheck, prefix) {
+			return true, string(str[len(prefix):])
+		}
+	}
+	return false, str
+}
+
+// StringTrimPreSuffix returns the string without the defined pre- and suffix
+func StringTrimPreSuffix(str string, preSuffix string) string {
+	if !(strings.HasPrefix(str, preSuffix) && strings.HasSuffix(str, preSuffix)) {
+		return str
+	}
+	return strings.TrimPrefix(strings.TrimSuffix(str, preSuffix), preSuffix)
+}
+
+// Equals provides a simple method to check whether or not 2 strings are equal
+func Equals(str1, str2 string, ignoreCase bool) bool {
+	if !ignoreCase {
+		return str1 == str2
+	}
+	return strings.ToLower(str1) == strings.ToLower(str2)
 }
